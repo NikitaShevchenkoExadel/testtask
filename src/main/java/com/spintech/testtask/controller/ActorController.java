aa719d915e613c4390594ca30b82acc57b27877e
shmy9001 <mykyta.shevchenko.consultant@nielsen.com>
2020-05-28 16:30:10 +0300

    Controllers Refactoring

--- src/main/java/com/spintech/testtask/controller/ActorController.java
+++ src/main/java/com/spintech/testtask/controller/ActorController.java
@@ -1,9 +1,9 @@
 package com.spintech.testtask.controller;
 
+import com.spintech.testtask.entity.actor.ActorUser;
 import com.spintech.testtask.entity.User;
-import com.spintech.testtask.entity.tvshow.TVShowUser;
-import com.spintech.testtask.service.TVShowService;
 import com.spintech.testtask.service.UserService;
+import com.spintech.testtask.service.ActorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -17,39 +17,39 @@ import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
-@RequestMapping("/tv")
-public class TVController {
+@RequestMapping("/person")
+public class ActorController {
     @Autowired
     private UserService userService;
 
     @Autowired
-    private TVShowService tvShowService;
+    private ActorService actorService;
 
-    @RequestMapping(value = "/markAsWatched/{tv_id}", method = POST)
-    public ResponseEntity markAsWatched(@PathVariable(value = "tv_id") Long id,
-                                        @RequestParam String email,
-                                        @RequestParam String password) {
+    @RequestMapping(value = "/makeFavorite/{actor_id}", method = POST)
+    public ResponseEntity actor(@PathVariable(value = "actor_id") Long id,
+                                  @RequestParam String email,
+                                  @RequestParam String password) {
         User user = userService.findUser(email, password);
         if (user == null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
 
-        TVShowUser tvShow = tvShowService.makeAsWatched(id, user);
+        ActorUser actor = actorService.makeFavorite(id, user);
 
-        return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON).body(tvShow);
+        return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON).body(actor);
     }
 
 
-    @RequestMapping(value = "/deleteFromWatched/{tv_id}", method = DELETE)
-    public ResponseEntity actorDelete(@PathVariable(value = "tv_id") Long id,
-                                      @RequestParam String email,
-                                      @RequestParam String password) {
+    @RequestMapping(value = "/deleteFavorite/{actor_id}", method = DELETE)
+    public ResponseEntity actorDelete(@PathVariable(value = "actor_id") Long id,
+                                @RequestParam String email,
+                                @RequestParam String password) {
         User user = userService.findUser(email, password);
         if (user == null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
 
-        tvShowService.deleteFromWatched(id, user);
+        actorService.deleteFavorite(id, user);
 
         return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON).build();
     }
